fd5f5fc784aabe47b34a43d84988ddc63936c2a4
marqrazz <marq.razz@gmail.com>
2023-05-01 21:42:38 -0600

    Add c3pzero packages

--- c3pzero_bringup/launch/c3pzero_sim.launch.py
+++ c3pzero_bringup/launch/c3pzero_sim.launch.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 # Author: Marq Rasmussen
 
+import shlex
 from launch import LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
@@ -35,22 +35,15 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "runtime_config_package",
-            default_value="c300_bringup",
+            default_value="c3pzero_bringup",
             description='Package with the controller\'s configuration in "config" folder. \
         Usually the argument is not set, it enables use of a custom setup.',
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "controllers_file",
-            default_value="c300_controllers.yaml",
-            description="YAML file with the controllers configuration.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
             "description_package",
-            default_value="c300_description",
+            default_value="c3pzero_description",
             description="Description package with robot URDF/XACRO files. Usually the argument \
         is not set, it enables use of a custom description.",
         )
@@ -58,14 +51,14 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_file",
-            default_value="c300_base.urdf",
+            default_value="c3pzero_kinova_gen3.xacro",
             description="URDF/XACRO description file with the robot.",
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
             "robot_name",
-            default_value="c300",
+            default_value="c3pzero",
             description="Robot name.",
         )
     )
@@ -78,25 +71,42 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "launch_rviz", default_value="true", description="Launch RViz?"
+            "jtc_controller",
+            default_value="joint_trajectory_controller",
+            description="Robot controller to start.",
         )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_pos_controller",
+            default_value="streaming_controller",
+            description="Robot controller to start.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_hand_controller",
+            default_value="robotiq_gripper_controller",
+            description="Robot hand controller to start.",
+        )
+    )    
+    declared_arguments.append(
+        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
     )
 
     # Initialize Arguments
     sim_ignition = LaunchConfiguration("sim_ignition")
     # General arguments
     runtime_config_package = LaunchConfiguration("runtime_config_package")
-    controllers_file = LaunchConfiguration("controllers_file")
     description_package = LaunchConfiguration("description_package")
     description_file = LaunchConfiguration("description_file")
     robot_name = LaunchConfiguration("robot_name")
     prefix = LaunchConfiguration("prefix")
     diff_drive_controller = LaunchConfiguration("diff_drive_controller")
+    robot_traj_controller = LaunchConfiguration("jtc_controller")
+    robot_pos_controller = LaunchConfiguration("robot_pos_controller")
+    # robot_hand_controller = LaunchConfiguration("robot_hand_controller")
     launch_rviz = LaunchConfiguration("launch_rviz")
-
-    robot_controllers = PathJoinSubstitution(
-        [FindPackageShare(runtime_config_package), "config", controllers_file]
-    )
 
     rviz_config_file = PathJoinSubstitution(
         [FindPackageShare(runtime_config_package), "rviz", "bringup_config.rviz"]
@@ -147,19 +157,37 @@ def generate_launch_description():
         )
     )
 
+    robot_traj_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[robot_traj_controller, "-c", "/controller_manager"],
+    )
+
     diff_drive_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=[diff_drive_controller, "-c", "/controller_manager"],
     )
 
+    # streaming controller
+    # robot_pos_controller_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=[robot_pos_controller, "-c", "/controller_manager"],
+    # )
+
+    # robot_hand_controller_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=[robot_hand_controller, "-c", "/controller_manager"],
+    # )
+
     ignition_spawn_entity = Node(
         package="ros_gz_sim",
         executable="create",
         output="screen",
         arguments=[
-            "-string",
-            robot_description_content,
+            "-string", robot_description_content,
             "-name", robot_name,
             "-allow_renaming", "true",
             "-x", "0.0",
@@ -177,19 +205,25 @@ def generate_launch_description():
             [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
         ),
         # TODO (marqrazz): fix the hardcoded path to the gazebo world
-        launch_arguments={
-            "ign_args": " -r -v 3 /root/c3pzero_ws/src/c3pzero/c300/c300_bringup/worlds/depot.sdf"
-        }.items(),
+        launch_arguments={"ign_args": " -r -v 3 /root/c3pzero_ws/src/c3pzero/c300/c300_bringup/worlds/depot.sdf"}.items(),
         condition=IfCondition(sim_ignition),
     )
 
     # Bridge
     camera_bridge = Node(
-        package="ros_ign_bridge",
-        executable="parameter_bridge",
+        package='ros_ign_bridge',
+        executable='parameter_bridge',
         # parameters=[{'use_sim_time': use_sim_time}],
-        arguments=["/base_scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan"],
-        output="screen",
+        arguments=['/rgbd_camera/image@sensor_msgs/msg/Image[ignition.msgs.Image',
+                   '/rgbd_camera/depth_image@sensor_msgs/msg/Image[ignition.msgs.Image',
+                   '/rgbd_camera/points@sensor_msgs/msg/PointCloud2[ignition.msgs.PointCloudPacked',
+                   '/rgbd_camera/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+                   '/segmentation/colored_map@sensor_msgs/msg/Image[ignition.msgs.Image',
+                #    '/segmentation/labels_map@sensor_msgs/msg/Image@ignition.msgs.Image',
+                   '/segmentation/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+                   '/base_scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan'
+                   ],
+        output='screen'
     )
 
     nodes_to_start = [
@@ -197,6 +231,9 @@ def generate_launch_description():
         joint_state_broadcaster_spawner,
         delay_rviz_after_joint_state_broadcaster_spawner,
         diff_drive_controller_spawner,
+        robot_traj_controller_spawner,
+        # robot_pos_controller_spawner,
+        # robot_hand_controller_spawner,
         ignition_launch_description,
         ignition_spawn_entity,
         camera_bridge,
---
+++ c3pzero_description/README.md
@@ -0,0 +1,2 @@
+# URDF for c3pzero
+differential drive robot (permobile c300) with Kinova Gen3 arm attached
