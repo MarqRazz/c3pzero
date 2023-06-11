faf9f6f29cc0d23e9fb926ca949ce6c4280804d5
marqrazz <marq.razz@gmail.com>
2023-06-11 10:36:28 -0600

    C300 Isaac configuration

---
+++ c300/c300_description/usd/python.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Copyright 2023 PickNik Inc.
+# All rights reserved.
+
+# Script to call the isaac launch system (see python.sh in the ISAAC_SCRIPT_DIR)
+
+ISAAC_SCRIPT_DIR="$HOME/.local/share/ov/pkg/isaac_sim-2022.2.1"
+
+# Prepend the path to all arguments passed in
+CUR_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NEW_ARGS=""
+for arg in "$@"
+do
+    NEW_ARGS="${NEW_ARGS} ${CUR_SCRIPT_DIR}/${arg}"
+done
+
+pushd ${ISAAC_SCRIPT_DIR}
+./python.sh $NEW_ARGS $CUR_SCRIPT_DIR
+popd
