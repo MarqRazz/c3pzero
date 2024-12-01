8662603953b75bc17b64ba1d81bec6413cfe83d7
marqrazz <marq.razz@gmail.com>
2024-11-30 17:37:46 -0700

    add wrist cameras

--- c300/c300_driver/setup.py
+++ c300/c300_driver/setup.py
@@ -8,7 +8,7 @@ package_name = "c300_driver"
 setup(
     name=package_name,
     version="0.0.1",
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
